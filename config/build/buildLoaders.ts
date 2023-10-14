56f8dadd692a6a0ff2c33d8b6af93388fe69807a
Tashu Soltamuradov <tashusoltamuradov@MacBook-Pro-Tashu.local>
2023-10-14 16:23:05 +0300

    выпелил sass, добавил tailwind и postCSS

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -10,7 +10,7 @@ export function buildLoaders({ isDev }: BuildOptions): RuleSetRule[] {
   };
 
   const cssLoader = {
-    test: /\.s[ac]ss$/i,
+    test: /\.css$/,
     use: [
       isDev ? "style-loader" : MiniCssExtractPlugin.loader,
       {
@@ -22,9 +22,10 @@ export function buildLoaders({ isDev }: BuildOptions): RuleSetRule[] {
               ? "[path][name]__[local]--[hash:base64:5]"
               : "[hash:base64:8]",
           },
+          importLoaders: 1
         },
       },
-      "sass-loader",
+      'postcss-loader',
     ],
   };
 
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,7 @@
 import { FC } from "react";
 
 const App: FC = () => {
-  return <div className="1234">Hello World</div>;
+  return <div>Hello World</div>;
 };
 
 export default App;
