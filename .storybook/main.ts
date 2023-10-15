40d721387b77f4d9f61a73d97733307aead91d7f
Tashu Soltamuradov <tashusoltamuradov@MacBook-Pro-Tashu.local>
2023-10-15 16:55:44 +0300

    Добавил storybook и историю UserCard компонент

---
+++ .storybook/main.ts
@@ -0,0 +1,41 @@
+import type { StorybookConfig } from "@storybook/react-webpack5";
+import path from "path";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+  webpackFinal: async (config) => {
+    if (config.module?.rules) {
+      config.module.rules.push({
+        test: /\.css$/,
+        use: [
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                ident: "postcss",
+                plugins: [require("tailwindcss"), require("autoprefixer")],
+              },
+            },
+          },
+        ],
+        include: path.resolve(__dirname, "../"),
+      });
+    }
+    return config;
+  },
+};
+
+export default config;
