658730c986b615eba9a26d49f590e863d3140822
Tashu Soltamuradov <tashusoltamuradov@MacBook-Pro-Tashu.local>
2023-10-17 19:34:32 +0300

    добавил тесты

--- src/shared/ui/Button/index.tsx
+++ src/shared/ui/Button/index.tsx
@@ -3,16 +3,20 @@ import { FC, ReactNode } from "react";
 type Props = {
   className?: string;
   children?: ReactNode;
+  disabled?: boolean;
 };
 
-const Button: FC<Props> = ({ className, children }) => {
+export const Button: FC<Props> = ({
+  className,
+  children,
+  disabled = false,
+}) => {
   return (
     <button
+      disabled={disabled}
       className={`px-10 py-4 rounded-40 bg-gradient-to-r from-gray-2 to-gray-3 text-black-3 text-xs font-bold ${className}`}
     >
       {children}
     </button>
   );
 };
-
-export default Button;
---
+++ src/shared/ui/ConnectionBlock/index.test.tsx
@@ -0,0 +1,11 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { ConnectionBlock } from ".";
+
+describe("ConnectionBlock", () => {
+  test("is there a title tag", () => {
+    render(<ConnectionBlock />);
+
+    expect(screen.getAllByRole("heading", { level: 3 })).toHaveLength(3);
+  });
+});
